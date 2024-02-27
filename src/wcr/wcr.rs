1419864c32110ebf472b2641bf669b47e336c39b
Kazuhiro-Mimaki <godblessu0405@gmail.com>
2024-02-27 19:12:16 +0900

    chapter5: run multiple files

--- src/wcr/wcr.rs
+++ src/wcr/wcr.rs
@@ -5,7 +5,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 struct Cli {
     #[arg(value_name = "PATH", help = "filepath to read from")]
-    path: PathBuf,
+    files: Vec<PathBuf>,
 
     #[arg(short = 'l', help = "Number of lines")]
     show_line_count: bool,
@@ -20,31 +20,33 @@ struct Cli {
 fn main() {
     let args = Cli::parse();
 
-    match fs::read_to_string(&args.path) {
-        Ok(content) => {
-            let (line_count, word_count, byte_count) = get_file_counts(content);
-            let mut count_vec = Vec::<String>::new();
-            if args.show_line_count {
-                count_vec.push(line_count.to_string());
-            }
-            if args.show_word_count {
-                count_vec.push(word_count.to_string());
-            }
-            if args.show_byte_count {
-                count_vec.push(byte_count.to_string());
-            }
-            if count_vec.len() == 0 {
-                count_vec.extend(vec![
-                    line_count.to_string(),
-                    word_count.to_string(),
-                    byte_count.to_string(),
-                ]);
-            }
+    for file in args.files {
+        match fs::read_to_string(&file) {
+            Ok(content) => {
+                let (line_count, word_count, byte_count) = get_file_counts(content);
+                let mut count_vec = Vec::<String>::new();
+                if args.show_line_count {
+                    count_vec.push(line_count.to_string());
+                }
+                if args.show_word_count {
+                    count_vec.push(word_count.to_string());
+                }
+                if args.show_byte_count {
+                    count_vec.push(byte_count.to_string());
+                }
+                if count_vec.len() == 0 {
+                    count_vec.extend(vec![
+                        line_count.to_string(),
+                        word_count.to_string(),
+                        byte_count.to_string(),
+                    ]);
+                }
 
-            let joined = count_vec.join(" ");
-            println!("{} {:?}", joined, args.path);
+                let joined = count_vec.join(" ");
+                println!("{} {:?}", joined, file);
+            }
+            Err(e) => panic!("There was a problem opening the file: {:?}", e),
         }
-        Err(e) => panic!("There was a problem opening the file: {:?}", e),
     }
 }
 
@@ -52,9 +54,11 @@ fn get_file_counts(content: String) -> (usize, usize, usize) {
     if content.len() == 0 {
         return (0, 0, 0);
     }
+
     let line_count = content.lines().count();
     let word_count = content.split_whitespace().count();
     let byte_count = content.len();
+
     return (line_count, word_count, byte_count);
 }
 
